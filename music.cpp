0310842cf9f851e5ae275a5fb91984e66cefccfc
bob <31841825+cixay@users.noreply.github.com>
2017-10-31 11:27:24 -0700

    Update music.cpp

--- music.cpp
+++ music.cpp
@@ -1,10 +1,21 @@
 /*
  * Alex Young
  * music
- * Last updated: 10/27/2017
+ * Last updated: 10/30/2017
  */
 
 #include <iostream>
 #include "music.h"
 
 using namespace std;
+
+music::music() {
+	cout << "Artist: ";
+	cin.get(artist, sizeof(artist));
+	cin.get();
+	cout << "Duration: ";
+	cin >> duration;
+	cout << "Publisher: ";
+	cin.get(publisher, sizeof(publisher));
+	cin.get();
+}
