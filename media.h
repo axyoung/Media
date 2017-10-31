08836eedc3fbea88ad99100eff382ac67334da87
bob <31841825+cixay@users.noreply.github.com>
2017-10-31 11:25:52 -0700

    Update media.h

--- media.h
+++ media.h
@@ -1,16 +1,22 @@
 /*
  * Alex Young
  * Header for media
- * Last updated: 10/27/2017
+ * Last updated: 10/30/2017
  */
 
 #ifndef MEDIA_H
+#define MEDIA_H
 #include <iostream>
 
 using namespace std;
 
 class media {
 	// media will be the parent file
+private:
+	char title[26];
+	int year;
+public:
+	media * create();
 	media();
 };
 
